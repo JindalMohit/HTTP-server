45f46310344907c84450dfd2d1e5e32c579c0348
JindalMohit <jindalmohit420@gmail.com>
2017-03-10 20:37:43 +0530

    added gui - start button working

--- client_socket1.py
+++ client_socket1.py
@@ -1,21 +1,29 @@
-# this is client 
 import socket
 
-# create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+class Client():
+	def __init__(self, port = 8000):
+		self.port = port
+		self.host = '172.25.14.62'
 
-# get local machine name
-host = socket.gethostname();
-port = 9999
+	def set_connection(self):
+		#create a socket
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+		self.socket.connect((self.host, self.port))
 
-# connection to hostname on the port
-s.connect((host, port))
+	def receive_msg(self):
+		"""msg = self.socket.recv(1024)
+		print(msg.decode('ascii'))"""
+		self.req_method = raw_input("Enter request or 'end' to end connection: ")
+		while self.req_method != 'end':
+			print(self.req_method)
+			self.socket.send(self.req_method)
+			self.req_method = raw_input("Enter request or 'end' to end connection: ")
+		self.end_connection()	
 
-# recieve a msg of n bytes
-n = 1024
-msg = s.recv(n)
+	def end_connection(self):
+		self.socket.close()
 
-# end the connection
-s.close()
+c = Client(8888)
+c.set_connection()
+c.receive_msg()
 
-print(msg.decode('ascii'))
--- server2.py
+++ server2.py
@@ -1,12 +1,17 @@
 # HTTP server using object-oriented approach
 import socket
 import sys
+from gi.repository import Gtk as gtk
+import threading
+from threading import Thread
 
 class Server(object):
 	"""docstring for Server"""
-	def __init__(self, port = 80):
-		self.host = '172.25.14.62'
+	def __init__(self, port = 8000):
+		self.host = '127.0.0.1'
 		self.port = port
+		self.output = ""
+		self.th = False
 
 	def activate_server(self):
 		#create a socket
@@ -16,30 +21,24 @@ class Server(object):
 		try:
 			self.socket.bind((self.host, self.port))
 			print("Launching HTTP server at ", self.host, ":", self.port)
+			self.output += "Launching HTTP server at " +str(self.host) + ":" + str(self.port)
 		except Exception as e:
-			print("User specified port is unavailable, trying to connect with port:80")
-			try:
-				self.port = 80
-				self.socket.bind((self.host, self.port))
-				print("Launching HTTP server at ", self.host, ":", self.port)
-			except Exception as e:
-				print("Port:", self.port, " is unavailable, trying to connect with port:8008")
-				try:
-					self.port = 8008
-					self.socket.bind((self.host, self.port))
-					print("Launching HTTP server at ", self.host, ":", self.port)
-				except Exception as e:
-					print("Port:", self.port, " is unavailable, trying to connect with port:8080")
-					try:
-						self.port = 8080
-						self.socket.bind((self.host, self.port))
-						print("Launching HTTP server at ", self.host, ":", self.port)
-					except Exception as e:
-						print("ERROR: Unable to assign a port to the socket.")
-						sys.exit()
+			print (e)
+			self.output += str(e)
+			sys.exit()
 		print("Server successfully acquired the socket with port:", self.port)
+		self.output += "Server successfully acquired the socket with port:" + str(self.port)
 		print("Press ctrl+c to close the server.")
+		self.output += "Press ctrl+c to close the server."
+		self.th = True
 		self.listen_client()
+
+	def deactivate_server(self):
+		if(self.th):
+			self.th = False
+
+	def is_activated(self):
+		return self.th
 
 	def listen_client(self):
 		# start TCP listen
@@ -51,12 +50,15 @@ class Server(object):
 			# accept the connection
 			c_socket, c_addr = self.socket.accept()
 			print("Got a connection from ", c_addr)
+			self.output += "Got a connection from " + str(c_addr)
 			#msg = "Thank you for connecting\n"
 			#c_socket.send(msg.encode('ascii'))
 
 			# extract the data from the client socket
+			print ("hello")
 			data = c_socket.recv(1024)
 			print(data.decode('ascii'))
+			self.output += data.decode('ascii')
 
 			self.handle_request(c_socket, data)
 
@@ -73,8 +75,15 @@ class Server(object):
 	def handle_request(self, client_socket, client_data):
 			data = client_data.split(" ")
 			request_method = data[0]
-			request_path = data[1]
-			http_version = data[2]
+			
+			try:
+				request_path = data[1]
+			except Exception as e:
+				print (e)
+			try:
+				http_version = data[2]
+			except Exception as e:
+				print (e)
 
 			if(request_method == 'GET'):
 				path = 'Resources' + request_path
@@ -90,8 +99,71 @@ class Server(object):
 				client_socket.send(msg)
 
 
-print("Starting HTTP server")
-# create a Server object
+class Gui():
+	def __init__(self):
+		self.frame = gtk.Window()
+		self.frame.set_default_size(600, 300)
+		self.frame.connect("delete-event", gtk.main_quit)
+		self.frame.set_title("HTTP-Server")
+		self.add_buttons()
+		self.place_widgets()
+
+	def add_buttons(self):
+		self.start_b = gtk.Button("Start")
+		self.start_b.set_size_request(70, 30)
+		self.start_b.connect("clicked", self.start_handler)
+		self.restart_b = gtk.Button("Restart")
+		self.restart_b.connect("clicked", self.restart_handler)
+		self.stop_b = gtk.Button("Stop")
+		self.stop_b.connect("clicked", self.stop_handler)
+		self.end_b = gtk.Button("End")
+		self.end_b.connect("clicked", self.end_handler)
+
+	def place_widgets(self, str="Welcome"):
+		self.vb = gtk.VBox(False, 1)
+		self.vb.add(self.start_b)
+		self.vb.add(self.restart_b)
+		self.vb.add(self.stop_b)
+		self.vb.add(self.end_b)
+
+		self.hb = gtk.HBox(False, 6)
+		self.hb.add(self.vb)
+		self.board = gtk.TextView()
+		self.hb.add(self.board)
+
+		self.text = gtk.TextBuffer()
+		# now the only task remaining is to add the content in text
+		#str = "Yipee! Finally working."
+		
+		#modify next 2 lines
+		#self.text.set_text(str.encode('utf-8'))
+		#self.board.set_buffer(self.text)
+
+		self.frame.add(self.hb)
+		self.frame.show_all()
+
+	def start_handler(self, widget):
+		if(s.is_activated()):
+			print("Server is already started")
+		else:
+			print("Starting HTTP server")
+			try:
+				thread_server.start()
+			except Exception as e:
+				print (e)
+	def restart_handler(self, widget):
+		pass
+	def stop_handler(self, widget):
+		if(s.is_activated()):
+			cond = threading.Condition()
+			while cond:
+				cond.wait()
+	def end_handler(self, widget):
+		sys.exit()
+
+
 s = Server(8888)
-# activate the Server
-s.activate_server()
+o1 = Gui()
+thread_gui = Thread(name='thread_gui', target=gtk.main, args=())
+thread_server = Thread(name='thread_server', target=s.activate_server, args=())
+thread_gui.start()
